86d9e5760bb811e87ffeea022dd0997e6b60411a
Bin Zhou <zhoubin@med-pool-0029.med.uni-augsburg.de>
2024-01-10 16:17:00 +0100

    modify dwd meta code

--- 2.code/era_download/down.sh
+++ 2.code/era_download/down.sh
@@ -3,17 +3,14 @@
 # module load anaconda/5.0.0
 # module load gnu 
 
-#declare -a vars=('u10','v10','d2','t2')
-declare -a vars=('v10')
+declare -a vars=('u10' 'v10' 'd2' 't2')
 
 # 
 for var in ${vars[*]}
     do
-        for year in {2018..2019}
-    		do 
-	       	   for month in {1..12}
+        for year in {2020..2022}
 	            # do echo $model+$var+$month
-		        do python3 down_unit.py $var $year $(printf "%02d" $month) &
-	       done
+		        # do python3 down_unit.py $var $year $(printf "%02d" $month) &
+              do python3 down_unit.py $var $year &
     done
 done
--- 2.code/era_download/down_unit.py
+++ 2.code/era_download/down_unit.py
@@ -24,51 +24,49 @@ if not Path(output_path).is_dir():
 
 
 ## get year and variable from I/O
-[variable, year, month]=sys.argv[1:]
+[variable, year]=sys.argv[1:]
 
 
-def era_download(variable,year,month):
+def era_download(variable,year):
     ## short names for variables   
     vdict={'u10':'10m_u_component_of_wind',
            'v10':'10m_v_component_of_wind',
            'd2':'2m_dewpoint_temperature',
-           't2':'2m_temperature'
-           } 
+           't2':'2m_temperature'} 
     
     varn=vdict[variable]
 
-    filename = output_path+variable+'_'+year+'_'+month+".zip"
+    filename = output_path+variable+'_'+year+".zip"
 
-    # number of days in the chosen year-month
-    n_days = calendar.monthrange(int(year),int(month))[1]
+    # months= 1-12, days=1-31, time= 00:00-23:00 
+    # CDS download available data for all days in each month
+    months= [str(i) for i in range(1,13)]
+    
+    days=[str(i) for i in range(1,32)]
+    
+    times=[f'{i:02d}:00' for i in range(0,24)]
+    
+    # # number of days in the chosen year-month
+    # n_days = calendar.monthrange(int(year),int(month))[1]
 
-    # array of day in char
-    days = [str(i) for i in range(1,n_days+1)]
+    # # array of day in char
+    # days = [str(i) for i in range(1,n_days+1)]
 
     r= c.retrieve(
       'reanalysis-era5-land',
       {
-        'time': [
-            '00:00', '01:00', '02:00',
-            '03:00', '04:00', '05:00',
-            '06:00', '07:00', '08:00',
-            '09:00', '10:00', '11:00',
-            '12:00', '13:00', '14:00',
-            '15:00', '16:00', '17:00',
-            '18:00', '19:00', '20:00',
-            '21:00', '22:00', '23:00',
-        ],
+        'time': times,
         'format': 'netcdf.zip',
         'year': year,
         'variable': varn,
-        'month': month,
-        'area':[51, 8.5, 47, 14], 
+        'month': months,
+        'area':[51, 8.5, 47, 14],  # bounding box for Bayern
         'day': days
       })
     r.download(filename)
 
 
 ## downloading variable for year....
-print('dowloading ', variable,' for year-month: ',year, month,'\n')
+print('dowloading', variable,'for year: ',year,'\n')
 
-era_download(variable,year, month)
+era_download(variable,year)
