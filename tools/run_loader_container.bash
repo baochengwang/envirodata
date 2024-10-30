8e36f1f32333fb65b91d169f03de7b34e8be9c21
Christoph Knote <christoph.knote@med.uni-augsburg.de>
2024-10-30 14:47:54 +0100

    Full data!

--- tools/run_loader_container.bash
+++ tools/run_loader_container.bash
@@ -2,4 +2,4 @@ docker network create -d bridge envirodata_network
 docker network connect envirodata_network nominatim
 docker network connect envirodata_network brightsky-web-1
 
-docker run --rm --mount source=envirodata_cache,target=/cache --network envirodata_network mbees/envirodata:latest load_data --services Noise_mapping config.yaml
+docker run --rm --mount source=envirodata_cache,target=/cache --network envirodata_network mbees/envirodata:latest load_data config.yaml
