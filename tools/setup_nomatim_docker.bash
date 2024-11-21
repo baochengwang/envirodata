a10e03595527bb2ba0b866fb81c9c00c8b3075b7
Christoph Knote <christoph.knote@med.uni-augsburg.de>
2024-11-21 17:41:59 +0100

    Version bump nominatim

--- tools/setup_nomatim_docker.bash
+++ tools/setup_nomatim_docker.bash
@@ -1,10 +1,10 @@
-# from https://github.com/mediagis/nominatim-docker/tree/master/4.4
+# from https://github.com/mediagis/nominatim-docker/tree/master/4.5
 
 docker run -it \
   -e PBF_URL=https://download.geofabrik.de/europe/germany-latest.osm.pbf \
   -e REPLICATION_URL=https://download.geofabrik.de/europe/germany-updates/ \
   -p 8080:8080 \
   --name nominatim \
-  mediagis/nominatim:4.4
+  mediagis/nominatim:4.5
 
 # nomatim pw for reference: qaIACxO6wMR3
