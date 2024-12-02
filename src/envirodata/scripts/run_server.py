7816b8cbed3b3364fe2c8830ede2975a5b78b670
Christoph Knote <christoph.knote@med.uni-augsburg.de>
2024-12-02 19:19:12 +0100

    UI rewrite

--- src/envirodata/scripts/run_server.py
+++ src/envirodata/scripts/run_server.py
@@ -8,9 +8,11 @@ from importlib.metadata import version
 from typing import Any
 from io import BytesIO
 import pytz
+from enum import Enum
+import math
 
 import uvicorn
-from fastapi import FastAPI, HTTPException, status, UploadFile, Request
+from fastapi import FastAPI, HTTPException, status, UploadFile, Request, BackgroundTasks
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import ORJSONResponse as JSONResponse
 from fastapi.responses import HTMLResponse, StreamingResponse
@@ -51,69 +53,170 @@ if end_date.tzinfo is None:
     end_date = end_date.replace(tzinfo=pytz.UTC)
 
 
+class ExcelJob:
+    class Status(Enum):
+        ERROR = "ERROR"
+        PENDING = "PENDING"
+        SUCCESS = "SUCCESS"
+        STARTED = "STARTED"
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.status = ExcelJob.Status.PENDING
+        self.percentDone = 0.0
+
+        self.buffer = BytesIO()
+
+    def run(self, contents):
+        try:
+            self.status = ExcelJob.Status.STARTED
+
+            data = BytesIO(contents)
+            try:
+                df = pd.read_excel(data, usecols=["id", "date", "address"])
+            except ValueError as exc:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc)
+                ) from exc
+
+            result = {}
+
+            i = 0
+            for idx, row in df.iterrows():
+                logger.info("Working on row %d of %d", i, len(df))
+                try:
+                    date = row["date"].tz_localize(pytz.utc)
+                    result[row["id"]] = _retrieve(date, row["address"])
+                except Exception as exc:
+                    result[row["id"]] = {"failed": str(exc)}
+                i += 1
+                self.percentDone = math.floor(float(i) / len(df) * 100.0)
+
+            flat = pd.DataFrame()
+            for id, envrow in result.items():
+                try:
+                    env = {"id": id}
+                    for service, data in envrow["environment"].items():
+                        env[service] = data["values"]
+
+                    env_pd = pd.json_normalize(env)
+                except KeyError:  # if getting env failed previously, make an empty row
+                    env_pd = pd.DataFrame.from_dict({0: {"id": id}}, orient="index")
+
+                flat = pd.concat([flat, env_pd])
+
+            self.buffer = BytesIO()
+            with pd.ExcelWriter(self.buffer) as writer:
+                flat.to_excel(writer, index=False)
+
+            self.percentDone = 0.0
+
+            self.status = ExcelJob.Status.SUCCESS
+        except Exception as e:
+            self.status = ExcelJob.Status.ERROR
+
+    def get_state(self):
+        if self.status == ExcelJob.Status.STARTED:
+            return self.percentDone
+        return self.status
+
+
+excelJob = ExcelJob()
+
+
+def _get_metadata(date: datetime.datetime) -> dict[str, Any]:
+    """Create basic metadata for response.
+
+    :param date: date requested
+    :type date: datetime.datetime
+    :return: basic metadata (package version, git commit, creation date)
+    :rtype: dict[str, Any]
+    """
+    metadata = {
+        "package_version": version("envirodata"),
+        "git_commit_hash": get_git_commit_hash(),
+        "creation_date": datetime.datetime.now(tz=datetime.timezone.utc).isoformat(),
+    }
+
+    metadata["requested_date_utc"] = date.isoformat()
+
+    return metadata
+
+
+def _retrieve(
+    date: datetime.datetime,
+    address: str,
+) -> dict[str, Any]:
+    """Retrieve environmental factors for a given date and address. (internal)
+
+    :param date: date requested
+    :type date: datetime.datetime
+    :param address: address requested
+    :type address: str
+    :raises HTTPException: address could not be geocoded
+    :return: exposure estimate
+    :rtype: dict[str, Any]
+    """
+    metadata = _get_metadata(date)
+
+    # (1) geocode address
+    try:
+        longitude, latitude = geocoder.geocode(address)
+    except IOError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Geocoding address failed: {str(exc)}",
+        ) from exc
+
+    geocoding = {
+        "address": address,
+        "location": {"longitude": longitude, "latitude": latitude},
+    }
+
+    # (2) get environmental factors
+    env = environment.get(date, longitude, latitude)
+
+    result = {"metadata": metadata, "geocoding": geocoding, "environment": env}
+
+    return result
+
+
+def validate_date(date: datetime.datetime) -> datetime.datetime:
+    if date.tzinfo is None:
+        logger.critical("Requested date not timezone-aware, assuming UTC!")
+        date = date.replace(tzinfo=pytz.UTC)
+
+    # (0) check date in range?
+    if (date < start_date) or (date > end_date):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Requested date out of cached range " f"({start_date} - {end_date})",
+        )
+    return date
+
+
 def main() -> None:
     """Envirodata REST-API."""
-
-    def _get_metadata(date: datetime.datetime) -> dict[str, Any]:
-        """Create basic metadata for response.
-
-        :param date: date requested
-        :type date: datetime.datetime
-        :return: basic metadata (package version, git commit, creation date)
-        :rtype: dict[str, Any]
-        """
-        metadata = {
-            "package_version": version("envirodata"),
-            "git_commit_hash": get_git_commit_hash(),
-            "creation_date": datetime.datetime.now(
-                tz=datetime.timezone.utc
-            ).isoformat(),
-        }
-
-        metadata["requested_date_utc"] = date.isoformat()
-
-        return metadata
-
-    def _retrieve(
-        date: datetime.datetime,
-        address: str,
-    ) -> dict[str, Any]:
-        """Retrieve environmental factors for a given date and address. (internal)
-
-        :param date: date requested
-        :type date: datetime.datetime
-        :param address: address requested
-        :type address: str
-        :raises HTTPException: address could not be geocoded
-        :return: exposure estimate
-        :rtype: dict[str, Any]
-        """
-        metadata = _get_metadata(date)
-
-        # (1) geocode address
-        try:
-            longitude, latitude = geocoder.geocode(address)
-        except IOError as exc:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Geocoding address failed: {str(exc)}",
-            ) from exc
-
-        geocoding = {
-            "address": address,
-            "location": {"longitude": longitude, "latitude": latitude},
-        }
-
-        # (2) get environmental factors
-        env = environment.get(date, longitude, latitude)
-
-        result = {"metadata": metadata, "geocoding": geocoding, "environment": env}
-
-        return result
 
     @app.get("/")
     def home(request: Request):
         return templates.TemplateResponse("home.html", context={"request": request})
+
+    @app.get("/manual")
+    def manual(request: Request):
+        return templates.TemplateResponse(
+            "manual.html",
+            context={"request": request, "metadata": environment.metadata()},
+        )
+
+    @app.get("/excel")
+    def excel(request: Request):
+        return templates.TemplateResponse(
+            "excel.html",
+            context={"request": request, "metadata": environment.metadata()},
+        )
 
     @app.get("/metadata")
     def metadata(request: Request):
@@ -122,11 +225,21 @@ def main() -> None:
             context={"request": request, "metadata": environment.metadata()},
         )
 
-    @app.get("/manual")
-    def retrieve(
-        date: datetime.datetime,
-        address: str,
-    ) -> JSONResponse:
+    @app.get("/api/html")
+    def api_html(
+        request: Request, date: datetime.datetime, address: str
+    ) -> HTMLResponse:
+        date = validate_date(date)
+
+        result = _retrieve(date, address)
+
+        return templates.TemplateResponse(
+            "result_table.html",
+            context={"request": request, "environment": result["environment"]},
+        )
+
+    @app.get("/api/simple")
+    def api_simple(date: datetime.datetime, address: str) -> JSONResponse:
         """Retrieve environmental factors for a given date and address.
 
         :param date: date requested
@@ -138,16 +251,7 @@ def main() -> None:
         :rtype: JSONResponse
         """
 
-        if date.tzinfo is None:
-            logger.critical("Requested date not timezone-aware, assuming UTC!")
-            date = date.replace(tzinfo=pytz.UTC)
-
-        # (0) check date in range?
-        if (date < start_date) or (date > end_date):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Requested date out of cached range ({start_date} - {end_date})",
-            )
+        date = validate_date(date)
 
         result = _retrieve(date, address)
 
@@ -156,84 +260,37 @@ def main() -> None:
 
         return JSONResponse(json_result)
 
-    @app.get("/manual_by_elements")
-    def retrieve_by_elements(
-        date: datetime.datetime,
-        postcode: str,
-        city: str,
-        streetname: str,
-        house_number: str = "",
-        extension: str = "",
-    ) -> JSONResponse:
-        """Retrieve environmental factors for a given date and address (as individual
-        elements).
+    @app.post("/api/excel/submit", status_code=status.HTTP_201_CREATED)
+    async def api_excel_submit(file: UploadFile, background_tasks: BackgroundTasks):
 
-        :param date: date requested
-        :type date: datetime.datetime
-        :param postcode: post code
-        :type postcode: str
-        :param city: city
-        :type city: str
-        :param streetname: street name
-        :type streetname: str
-        :param house_number: house number, defaults to ""
-        :type house_number: str, optional
-        :param extension: address extension, defaults to ""
-        :type extension: str, optional
-        :return: exposure estimate
-        :rtype: JSONResponse
-        """
-        address = geocoder.standardize_address(
-            postcode, city, streetname, house_number, extension
-        )
-        return retrieve(date, address)
-
-    @app.post("/excel")
-    async def create_upload_file(file: UploadFile) -> StreamingResponse:
+        if excelJob.status == ExcelJob.Status.STARTED:
+            return {"message": "Job is running"}
 
         contents = file.file.read()
-        data = BytesIO(contents)
-        try:
-            df = pd.read_excel(data, usecols=["id", "date", "address"])
-        except ValueError as exc:
+        background_tasks.add_task(excelJob.run, contents)
+        return {"message": "Job created"}
+
+    @app.get("/api/excel/status", status_code=status.HTTP_200_OK)
+    def api_excel_status():
+        return excelJob.get_state()
+
+    @app.get("/api/excel/get")
+    def api_excel_get() -> StreamingResponse:
+        if not excelJob.get_state() == excelJob.Status.SUCCESS:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc)
+                status_code=status.HTTP_425_TOO_EARLY, detail="Result not ready yet!"
             )
 
-        result = {}
-
-        i = 0
-        for idx, row in df.iterrows():
-            logger.info("Working on row %d of %d", i, len(df))
-            try:
-                date = row["date"].tz_localize(pytz.utc)
-                result[row["id"]] = _retrieve(date, row["address"])
-            except Exception as exc:
-                result[row["id"]] = {"failed": str(exc)}
-            i += 1
-
-        flat = pd.DataFrame()
-        for id, envrow in result.items():
-            try:
-                env = {"id": id}
-                for service, data in envrow["environment"].items():
-                    env[service] = data["values"]
-
-                env_pd = pd.json_normalize(env)
-            except KeyError:  # if getting env failed previously, make an empty row
-                env_pd = pd.DataFrame.from_dict({0: {"id": id}}, orient="index")
-
-            flat = pd.concat([flat, env_pd])
-
-        buffer = BytesIO()
-        with pd.ExcelWriter(buffer) as writer:
-            flat.to_excel(writer, index=False)
-
         return StreamingResponse(
-            BytesIO(buffer.getvalue()),
+            BytesIO(excelJob.buffer.getvalue()),
             media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             headers={"Content-Disposition": "attachment; filename=environment.xlsx"},
         )
+
+    @app.get("/api/excel/reset", status_code=status.HTTP_200_OK)
+    def api_excel_reset():
+        excelJob.reset()
+        return True
 
     uvicorn_config = uvicorn.Config(app, **config["uvicorn"])
     uvicorn_server = uvicorn.Server(uvicorn_config)
