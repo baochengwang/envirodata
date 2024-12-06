389abedcb8be4435573b918376649e56cb51820c
Christoph Knote <christoph.knote@med.uni-augsburg.de>
2024-12-06 10:28:28 +0100

    Geocode for all of Germany by default

--- deploy/00_setup_nomatim_docker.bash
+++ deploy/00_setup_nomatim_docker.bash
@@ -2,7 +2,7 @@
 
 # REPLICATION_URL omitted intentionally, breaks offline...
 docker run -it \
-  -e PBF_URL=https://download.geofabrik.de/europe/germany/bayern-latest.osm.pbf \
+  -e PBF_URL=https://download.geofabrik.de/europe/germany-latest.osm.pbf \
   -p 8080:8080 \
   --name nominatim \
   mediagis/nominatim:4.5
