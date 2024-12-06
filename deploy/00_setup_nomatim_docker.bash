8d0d8498d065677c8ecbdccf339eb00d678ec59d
Christoph Knote <christoph.knote@med.uni-augsburg.de>
2024-12-06 14:01:04 +0100

    Nominatim updates

--- deploy/00_setup_nomatim_docker.bash
+++ deploy/00_setup_nomatim_docker.bash
@@ -1,9 +1,11 @@
 # from https://github.com/mediagis/nominatim-docker/tree/master/4.5
 
 # REPLICATION_URL omitted intentionally, breaks offline...
-docker run -it \
+docker run -it --shm-size=1g \
   -e PBF_URL=https://download.geofabrik.de/europe/germany-latest.osm.pbf \
+  -e FREEZE=true \
   -p 8080:8080 \
+  -v nominatim-data:/var/lib/postgresql/16/main \
   --name nominatim \
   mediagis/nominatim:4.5
 
